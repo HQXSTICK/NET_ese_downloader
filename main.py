26a01f0c6f68d1052ebcc46fa796ab91ee51e480
HQXAKIOI <64624915+HQXSTICK@users.noreply.github.com>
2022-01-24 22:52:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+# version 1.0.1
+# author HQXSTICK
+
 import requests
 import re
 from multiprocessing import Pool
 headers = {
     'Referer': 'https://music.163.com/',
     "User-Agent": "Chrome/97.0.4692.99 "
-    # 在这里可以更改你的浏览器配置
+    # 在这里需要更改你的浏览器配置，下面是一些格式
+    # Chrome/版本
+    # Mozilla/版本
 }
 
 
 def getlist():
     print("please type your musiclist number here:")
     id = input()
-    getsongs(id)
+    print("please type the way to the folder you want to download:")
+    folder = input()
+    getsongs(id, folder)
 
 
-def getsongs(id):
+def getsongs(id, folder):
     playlist_url = "https://music.163.com/playlist?id=%s" % id
     res = requests.get(playlist_url, headers=headers)
     mylog = open('html.html', mode='a', encoding='utf-8')
@@ -23,7 +30,7 @@ def getsongs(id):
     for i in re.findall(r'<a href="/song\?id=(\d+)">(.*?)</a>', res.text):
         download_url = "http://music.163.com/song/media/outer/url?id=%s" % i[0]
         try:
-            with open('music/'+i[1]+'.mp3', 'wb')as f:
+            with open("%s/" % folder+i[1]+'.mp3', 'wb')as f:
                 f.write(requests.get(download_url, headers=headers).content)
         except FileNotFoundError:
             pass
